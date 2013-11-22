660b8669f6d0a726d1c815b8696076b5d7791364
disterics <git@wojeshun.net>
2013-11-21 23:51:31 -0800

    First commit. Adding auto-generated gem files.

---
+++ lib/guard/cmake.rb
@@ -0,0 +1,7 @@
+require "guard/cmake/version"
+
+module Guard
+  module Cmake
+    # Your code goes here...
+  end
+end
---
+++ lib/guard/cmake/version.rb
@@ -0,0 +1,5 @@
+module Guard
+  module Cmake
+    VERSION = "0.0.1"
+  end
+end
