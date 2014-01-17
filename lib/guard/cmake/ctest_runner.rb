81ba0cbf92c29f7919deda21051138e2aa35c28d
disterics <git@wojeshun.net>
2014-01-16 20:17:03 -0800

    When executing a test, run make in test directory if the changed file is not a test file.

--- lib/guard/cmake/ctest_runner.rb
+++ lib/guard/cmake/ctest_runner.rb
@@ -6,6 +6,7 @@ module Guard
     class CTestRunner < CommandRunner
 
       require 'guard/cmake/ctest_command'
+      require 'guard/cmake/make_command'
 
       def run_all
         _run(true)
@@ -28,20 +29,47 @@ module Guard
       end
 
       def _ctest(path)
+        result = _build(path)
+        _test(path) if result
+      end
+
+      def _build(path)
+        result = true
+        return result if path.nil?
+        result = _execute(MakeCommand.new(path.test)) unless _test_path?(path.path)
+        result
+      end
+
+      def _test(path)
+        path = path.nil? ? nil : path.test
         command = CTestCommand.new(path)
         _execute(command)
       end
 
       def _clean_paths(paths)
         dirs = _get_directories(paths)
-        if @options[:ctest_prefix]
-          dirs.map! do | dir |
-            parts = dir.split(File::SEPARATOR)
-            test_dir = File.join(@options[:ctest_prefix], parts[1..-1])
-            test_dir if _exists_in_build_dir?(test_dir)
-          end
+        dirs.map! do | dir |
+          test_dir = to_test_path(dir)
+          OpenStruct.new({:path => dir, :test => test_dir}) if _exists_in_build_dir?(test_dir)
         end
         dirs.compact
+      end
+
+      def to_test_path(path)
+        dir = path
+        if @options[:ctest_prefix]
+          parts = dir.split(File::SEPARATOR)
+          dir = File.join(@options[:ctest_prefix], parts[1..-1])
+        end
+        dir
+      end
+
+      def _test_path?(path)
+        result = false
+        if @options[:ctest_prefix]
+          result = (path =~ /^#{@options[:ctest_prefix]}/)
+        end
+        result
       end
 
     end
--- spec/lib/guard/cmake/ctest_runner_spec.rb
+++ spec/lib/guard/cmake/ctest_runner_spec.rb
@@ -6,6 +6,7 @@ describe Guard::CMake::CTestRunner do
 
   let(:build_dir) { 'build' }
   let(:command) { double('command', :title => 'command' ) }
+  let(:make_command) { double('command', :title => 'make' ) }
   let(:full_build_path) { File.join(project_dir, build_dir) }
   let(:project_dir) { 'tmp/project1' }
 
@@ -13,6 +14,7 @@ describe Guard::CMake::CTestRunner do
   before {
     allow(Kernel).to receive(:system) { true }
     allow(Guard::CMake::CTestCommand).to receive(:new) { command }
+    allow(Guard::CMake::MakeCommand).to receive(:new) { make_command }
     allow(Guard::Notifier).to receive(:notify) { true }
     allow(Dir).to receive(:chdir) { true }
   }
@@ -26,7 +28,7 @@ describe Guard::CMake::CTestRunner do
 
   describe "#run_all" do
 
-    it "builds command with no directory" do
+    it "builds ctest command with no directory" do
       expect(Guard::CMake::CTestCommand).to receive(:new).with(nil).exactly(1).times.and_return(command)
       subject.run_all
     end
@@ -41,7 +43,7 @@ describe Guard::CMake::CTestRunner do
   describe "#run" do
     let(:paths) { %w[src/path1/a.cpp src/path2/b.h] }
 
-    it "builds command with each directory" do
+    it "builds ctest command with each directory" do
       expect(Guard::CMake::CTestCommand).to receive(:new).with(File.dirname(paths[0])).ordered.and_return(command)
       expect(Guard::CMake::CTestCommand).to receive(:new).with(File.dirname(paths[1])).ordered.and_return(command)
       subject.run(paths)
@@ -64,19 +66,48 @@ describe Guard::CMake::CTestRunner do
         Dir.chdir(full_build_path) { FileUtils.makedirs(test_paths) }
       }
 
-      it "creates command with each directory prefixed with :ctest_prefix one level deep" do
-        expect(Guard::CMake::CTestCommand).to receive(:new).with(test_paths[0]).ordered.and_return(command)
-        expect(Guard::CMake::CTestCommand).to receive(:new).with(test_paths[1]).ordered.and_return(command)
-        subject.run(paths)
+      context "with paths outside ctest_prefix" do
+        it "creates ctest command with each directory prefixed with :ctest_prefix one level deep" do
+          expect(Guard::CMake::CTestCommand).to receive(:new).with(test_paths[0]).ordered.and_return(command)
+          expect(Guard::CMake::CTestCommand).to receive(:new).with(test_paths[1]).ordered.and_return(command)
+          subject.run(paths)
+        end
+
+        it "creates make command with each directory prefixed with :ctest_prefix one level deep" do
+          expect(Guard::CMake::MakeCommand).to receive(:new).with(test_paths[0]).ordered.and_return(make_command)
+          expect(Guard::CMake::MakeCommand).to receive(:new).with(test_paths[1]).ordered.and_return(make_command)
+          subject.run(paths)
+        end
+      end
+
+      context "with paths inside ctest_prefix" do
+        let(:paths) { %w[test/path1/a.cpp test/path2/b.h] }
+
+        it "creates ctest command with each directory" do
+          expect(Guard::CMake::CTestCommand).to receive(:new).with(File.dirname(paths[0])).ordered.and_return(command)
+          expect(Guard::CMake::CTestCommand).to receive(:new).with(File.dirname(paths[1])).ordered.and_return(command)
+          subject.run(paths)
+        end
+
+        it "does not create make commands" do
+          expect(Guard::CMake::MakeCommand).to_not receive(:new)
+          subject.run(paths)
+        end
       end
 
       context "with missing target folder" do
         let(:paths) { %w[src/path3/a.cpp src/path4/b.h] }
 
-        it "does not create commands for missing target directories" do
+        it "does not create ctest commands for missing target directories" do
           expect(Guard::CMake::CTestCommand).to_not receive(:new)
           subject.run(paths)
         end
+
+        it "does not create make commands for missing target directories" do
+          expect(Guard::CMake::MakeCommand).to_not receive(:new)
+          subject.run(paths)
+        end
+
       end
 
     end
